bacebcd54352e835eba96d55e493766814eea68c
Xiao Liu <liuxiao.qmbupt@gmail.com>
2019-09-12 23:55:57 -0700

    Xcode AR project template

---
+++ maui/mauiTests/mauiTests.swift
@@ -0,0 +1,34 @@
+//
+//  mauiTests.swift
+//  mauiTests
+//
+//  Created by Xiao Liu on 9/12/19.
+//  Copyright © 2019 fly. All rights reserved.
+//
+
+import XCTest
+@testable import maui
+
+class mauiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
